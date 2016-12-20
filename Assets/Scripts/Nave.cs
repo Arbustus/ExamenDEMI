81afeb6e594afd38ebbd9aff0287a215f17df090
Arbustus <jm.martinezroa@gmail.com>
2016-12-20 19:13:18 +0100

    10. Nave
    No logro hacer que gire, se que es un vector tres ya que uso el eje z
    pero no recuerdo y no tengo tiempo para hacer que suceda.

---
+++ Assets/Scripts/Nave.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nave : MonoBehaviour {
+	private Rigidbody2D rb;
+	public float fuerza;
+	void Start(){
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update(){
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			rb.AddForce (Vector2.up * fuerza);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rb.AddForce (Vector2.right * fuerza);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rb.AddForce (Vector2.right * -fuerza);
+		}
+	}
+	void OnCollisionEnter2D (Collision2D col){
+		if(col.gameObject.tag == "muerte"){
+			Destroy(gameObject);
+		}
+	}
+
+}

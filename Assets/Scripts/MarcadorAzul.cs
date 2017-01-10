d3e4e927f4a6cff301be64d8e84af0decb9a685c
José Manuel Martínez Roa <jm.martinezroa@gmail.com>
2017-01-10 20:44:17 +0100

    Completar
    Se ha acabado el nivel del futbol

---
+++ Assets/Scripts/MarcadorAzul.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MarcadorAzul : MonoBehaviour {
+	public Text texto_marcador;
+	int goles = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.transform.name == "Pelota") {
+			goles += 1; //goles = goles +1
+			texto_marcador.text = "Azul " + goles;
+		}
+	}
+}
